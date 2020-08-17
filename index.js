a5e3d56505eee760676f1d550191140de3ca716a
Taylor C. MacDonald <taylor@helloample.com>
2020-08-17 16:09:23 -0400

    Add entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const Replace = require("./lib/replace")
+const Utils = require("./lib/utils")
+
 module.exports = {
-  onPostBuild: () => {
-    console.log('hello world')
+  onPostBuild: async ({ constants, inputs, utils: { status } }) => {
+    const results = await new Replace(constants.PUBLISH_DIR, inputs).perform()
+    let replacements = new Set()
+
+    for (let file in results) {
+      Utils.log(file, true)
+      results[file].results.forEach(result => {
+        Utils.log(`\t(${result.numReplacements}) ${result.from}=${result.to}`, true)
+        replacements.add(result.from)
+      })
+      Utils.log("\n")
+    }
+
+    let n = Object.keys(results).length
+    status.show({
+      summary: `Replaced ${replacements.size} ${Utils.pluralize(
+        "variable",
+        replacements.size
+      )} across ${n} ${Utils.pluralize("file", n)}.`
+    })
   }
 }
